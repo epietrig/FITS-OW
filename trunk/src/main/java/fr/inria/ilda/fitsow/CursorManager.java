15bcb5ba1ed764a4347a55db77ad62839a8b6b3e
epietrig <emmanuel.pietriga@inria.fr>
2015-06-15 15:06:28 +0200

    Adding an additional layer for cursors

---
+++ trunk/src/main/java/fr/inria/ilda/fitsow/CursorManager.java
@@ -0,0 +1,17 @@
+/*   Copyright (c) INRIA, 2015. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file LICENSE.
+ *
+ * $Id: $
+ */
+
+package fr.inria.ilda.fitsow;
+
+public class CursorManager {
+
+    FITSOW app;
+
+    CursorManager(FITSOW app){
+        this.app = app;
+    }
+
+}
