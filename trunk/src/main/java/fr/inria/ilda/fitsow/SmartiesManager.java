af9d43736f077846b27f9f81cc67819051b5d5c4
Olivier Chapuis <chapuis@lri.fr>
2015-06-18 08:05:33 +0200

    smarties manager use the cursor manager

--- trunk/src/main/java/fr/inria/ilda/fitsow/SmartiesManager.java
+++ trunk/src/main/java/fr/inria/ilda/fitsow/SmartiesManager.java
@@ -3,19 +3,12 @@ package fr.inria.ilda.fitsow;
 import java.awt.geom.Point2D;
 import java.awt.Color;
 
-//import fr.inria.zuist.cluster.viewer.WallCursor;
 import java.util.Vector;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import fr.inria.zvtm.animation.Animation;
-import fr.inria.zvtm.animation.EndAction;
-
-import fr.inria.ilda.gesture.BasicSegmenter;
 import fr.inria.ilda.gesture.GestureManager;
-import fr.inria.ilda.gestures.MTRecognitionEngine;
-//import fr.inria.ilda.gesture.RecognitionLayer;
 import fr.inria.ilda.smarties.SmartiesDeviceGestures;
 
 import fr.lri.smarties.libserver.Smarties;
@@ -29,17 +22,14 @@ import fr.lri.smarties.libserver.SmartiesWidgetHandler;
 import java.util.Observer;
 import java.util.Observable;
 
-import fr.inria.zvtm.glyphs.Glyph;
-import fr.inria.zvtm.glyphs.VText;
-import fr.inria.zvtm.engine.Utils;
-
-import fr.inria.ilda.fitsow.cursors.OlivierCursor;
 
 class SmartiesManager implements Observer {
 
 	FITSOW application;
 	Navigation nav;
 	Smarties smarties;
+	CursorManager inputManager;
+
 	int countWidget;
 	// gesture
 	HashMap<SmartiesDevice, SmartiesDeviceGestures> devGesturesMap = new HashMap<SmartiesDevice, SmartiesDeviceGestures>();
@@ -50,6 +40,8 @@ class SmartiesManager implements Observer {
 		this.application = app;
 		this.nav = application.getNavigation();
 	    this.smarties = new Smarties(app_width, app_height, col, row);
+	    this.inputManager = app.cm;
+
 		System.out.println("new Smarties "+app_width+" "+app_height+" "+col+" "+row);
 	    
 	    smarties.initWidgets(12,6);
@@ -63,37 +55,6 @@ class SmartiesManager implements Observer {
 		
 	    smarties.Run();
 	}
-
-	class myCursor{
-
-        public int id;
-        public double x, y;
-        public Color color;
-        public OlivierCursor wc;
-        public Point2D.Double delta;
-
-        public void move(double x, double y){
-            this.x = x; this.y = y;
-            wc.moveTo((long)(x*application.getDisplayWidth() - application.getDisplayWidth()/2.0), (long)(application.getDisplayHeight()/2.0 - y*application.getDisplayHeight()));
-        }
-
-        public myCursor(int id, double x, double y){
-            this.id = id;
-            this.x = x;
-            this.y = y;
-            this.color = SmartiesColors.getPuckColorById(id);
-
-            wc = new OlivierCursor(
-                application.getCursorSpace(),
-                (application.runningOnWall()) ? 20 : 2, (application.runningOnWall()) ? 150 : 10,
-                this.color);
-            move(x, y);
-        }
-
-    } // class myCursor
-
-    // ilda gesture stuff
-
 
 	public void update(Observable obj, Object arg)
 	{
@@ -146,9 +107,8 @@ class SmartiesManager implements Observer {
 			}
         	case SmartiesEvent.SMARTIE_EVENTS_TYPE_CREATE:{
                 //System.out.println("Create Puck: " + se.id);
-                se.p.app_data = new myCursor(se.p.id, se.p.x, se.p.y);
-                myCursor c = (myCursor)se.p.app_data;
-                c.wc.setVisible(true);
+                inputManager.createCursor(
+                    this, se.id, se.p.x, se.p.y, SmartiesColors.getPuckColorById(se.id));
                 break;
             }
             case SmartiesEvent.SMARTIE_EVENTS_TYPE_SELECT:{
@@ -159,24 +119,21 @@ class SmartiesManager implements Observer {
             case SmartiesEvent.SMARTIE_EVENTS_TYPE_STORE:{
                 //repaint();
                 if (se.p != null){
-                    myCursor c = (myCursor)se.p.app_data;
-                    c.wc.setVisible(false);
-                    //_repaintCursor(c);
+                	inputManager.hideCursor(this, se.id);
                 }
                 break;  
             }
             case SmartiesEvent.SMARTIE_EVENTS_TYPE_UNSTORE:{
                 if (se.p != null){
-                    myCursor c = (myCursor)se.p.app_data;
-                    c.move(se.p.x, se.p.y);
-                    c.wc.setVisible(true);
+                	inputManager.showCursor(this, se.id);
                 }
                 break;
             }
             case SmartiesEvent.SMARTIE_EVENTS_TYPE_DELETE:{
-                myCursor c = (myCursor)se.p.app_data;
-                c.wc.dispose();
-                smarties.deletePuck(se.p.id);
+                if (se.p != null){
+                	inputManager.removeCursor(this, se.id);
+                	smarties.deletePuck(se.p.id);
+                }
                 break;
             }
         	case SmartiesEvent.SMARTIE_EVENTS_TYPE_START_MOVE:{
@@ -190,9 +147,7 @@ class SmartiesManager implements Observer {
             case SmartiesEvent.SMARTIE_EVENTS_TYPE_MOVE:{
                 //System.out.println("SMARTIE_EVENTS_TYPE_MOVE");
                 if (se.p != null){
-                    myCursor c = (myCursor)se.p.app_data;
-                    c.move(se.p.x, se.p.y);
-
+                    inputManager.moveCursorTo(this, se.id, se.p.x, se.p.y);
                     if (se.mode == SmartiesEvent.SMARTIE_GESTUREMOD_DRAG){
                     }
                 }
