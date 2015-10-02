c3fdd73760949552ab0ea511dda0ceeb659d6d59
Amanda Ibsen <amanda.ibsen@gmail.com>
2015-10-02 16:08:18 -0300

    list of results

--- trunk/src/main/java/fr/inria/ilda/simbad/SimbadResults.java
+++ trunk/src/main/java/fr/inria/ilda/simbad/SimbadResults.java
@@ -11,14 +11,25 @@ public class SimbadResults extends Composite{
 
   private int size, w, h;
   private VRectangle background;
+  private VText[] ids;
 
   public SimbadResults(List<AstroObject> results, double x, double y){
     size = results.size();
-    h = size*20;
+    h = size*20+5;
     w = 200;
-    background = new VRectangle (x, y, 0, 200, 200, Color.red);
+    background = new VRectangle (x, y, 0, w, h, Color.gray);
     background.setVisible(true);
     this.addChild(background);
+    double start = background.getBounds()[1];
+    ids = new VText[size];
+    VSegment[] splits = new VSegment[size];
+    for(int i = 0; i <size; i++){
+      ids[i] = new VText(x-x/2+5, start-20*(i+1), 0, Color.white, results.get(i).getIdentifier());
+      ids[i].setVisible(true);
+      this.addChild(ids[i]);
+      splits[i] = new VSegment(x-x/2, start-20*(i+1)-5, x+x/2, start-20*(i+1)-5, 0, Color.black);
+      this.addChild(splits[i]);
+    }
     this.setVisible(true);
   }
 }
