8bc33ad7d141b166169a3f5f403d5fe6a3b1d9d6
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2016-03-17 12:30:53 -0300

    updated JAR files

--- trunk/wilder_all_run.sh
+++ trunk/wilder_all_run.sh
@@ -78,7 +78,7 @@ echo "LIP: " $LIP
 JARS="target/commons-logging-1.1.jar"
 JARS=$JARS":target/args4j-2.0.29.jar"
 JARS=$JARS":target/aspectjrt-1.8.6.jar"
-JARS=$JARS":target/jgroups-2.7.0.GA.jar"
+JARS=$JARS":target/jgroups-3.6.6.Final.jar"
 JARS=$JARS":target/log4j-1.2.17.jar"
 JARS=$JARS":target/slf4j-api-1.7.10.jar"
 JARS=$JARS":target/slf4j-log4j12-1.7.10.jar"
@@ -137,7 +137,7 @@ done
 
 sleep 3
 JARS="target/aspectjrt-1.8.6.jar"
-JARS=$JARS":target/jgroups-2.7.0.GA.jar"
+JARS=$JARS":target/jgroups-3.6.6.Final.jar"
 JARS=$JARS":target/log4j-1.2.17.jar"
 JARS=$JARS":target/slf4j-api-1.7.10.jar"
 JARS=$JARS":target/slf4j-log4j12-1.7.10.jar"
