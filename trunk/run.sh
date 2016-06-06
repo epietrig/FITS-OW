1e5bf3541d56f747a28662b00bd304c32a6480bc
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2016-06-06 20:47:59 +0200

    Upgrade to JEP 3.5.3

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx1024M -Xms512M -Djava.library.path=/home/aibsen/Virtualenvs/fits-ow/local/lib/python2.7/site-packages/jep -Dcom.sun.media.jai.disableMediaLib=true -jar target/fits-ow-0.1.jar "$@"
+java -Xmx1024M -Xms512M -Djava.library.path=/Library/Python/2.7/site-packages/jep -Dcom.sun.media.jai.disableMediaLib=true -jar target/fits-ow-0.1.jar "$@"
--- trunk/single_master_run.sh
+++ trunk/single_master_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #IP="192.168.6.90"
-IP="192.168.1.10"
+#IP="192.168.1.10"
 #IP="129.175.5.15"
-
+IP="127.0.0.1"
 
 JARS="target/aspectjrt-1.8.9.jar"
 JARS=$JARS":target/jgroups-3.6.8.Final.jar"
@@ -17,6 +17,7 @@ JARS=$JARS":target/xmlParserAPIs-2.6.2.jar"
 JARS=$JARS":target/zvtm-svg-0.2.2-SNAPSHOT.jar"
 JARS=$JARS":target/commons-logging-1.1.jar"
 JARS=$JARS":target/args4j-2.0.29.jar"
+JARS=$JARS":target/jep-3.5.3.jar"
 JARS=$JARS":target/fits-ow-0.1.jar"
 
 java -server -Djava.net.preferIPv4Stack=true -Djava.library.path=/Library/Python/2.7/site-packages/jep -Djgroups.bind_addr=$IP -Dcom.sun.media.jai.disableMediaLib=true -Xmx1g -cp .:$JARS fr.inria.ilda.fitsow.WallFITSOW -r 1 -c 1 -bw 800 -bh 600 "$@"
